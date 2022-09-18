24d4d2e2f26756ab263f246c7e08a3145c55dc5e
Mallory Daniel <mllr.dnl@gmail.com>
2022-09-18 12:39:56 -0700

    finished tutorial

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,12 +1,15 @@
 import { useRouter } from "next/router";
-
+import { server } from "../../../config/index.js";
 import Link from "next/link";
+import Meta from "../../../components/Meta.js";
+
 const article = ({ article }) => {
   // const router = useRouter();
   // const { id } = router.query;
 
   return (
     <>
+      <Meta title={article.title} description={article.excerpt} />
       <h1>{article.title}</h1>
       <p>{article.body}</p>
       <br />
@@ -16,9 +19,7 @@ const article = ({ article }) => {
 };
 
 export const getStaticProps = async (context) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-  );
+  const res = await fetch(`${server}/api/articles/${context.params.id}`);
 
   const article = await res.json();
 
@@ -30,7 +31,7 @@ export const getStaticProps = async (context) => {
 };
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const res = await fetch(`${server}/api/articles/`);
 
   const articles = await res.json();
 
@@ -42,5 +43,33 @@ export const getStaticPaths = async () => {
     fallback: false,
   };
 };
+
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+
+//   const article = await res.json();
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+//   const articles = await res.json();
+
+//   const ids = articles.map((article) => article.id);
+//   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
 export default article;
